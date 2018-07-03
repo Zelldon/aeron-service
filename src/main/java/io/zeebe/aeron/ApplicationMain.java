e77a9a46211f016209392666f8a42318585e498a
Philipp Ossler <philipp.ossler@gmail.com>
2018-07-03 11:42:48 +0200

    wip

--- src/main/java/io/zeebe/aeron/ApplicationMain.java
+++ src/main/java/io/zeebe/aeron/ApplicationMain.java
@@ -1,7 +1,9 @@
 package io.zeebe.aeron;
 
+import io.aeron.CommonContext;
 import io.aeron.archive.Archive;
 import io.aeron.archive.ArchiveThreadingMode;
+import io.aeron.archive.client.AeronArchive;
 import io.aeron.cluster.ClusteredMediaDriver;
 import io.aeron.cluster.ConsensusModule;
 import io.aeron.cluster.service.ClusteredServiceContainer;
@@ -10,76 +12,157 @@ import io.aeron.driver.ThreadingMode;
 import io.zeebe.aeron.client.JobClient;
 import io.zeebe.aeron.client.ZbClient;
 import io.zeebe.aeron.service.JobCreateService;
+
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 import org.agrona.CloseHelper;
 
 public class ApplicationMain {
 
-    private static final long MAX_CATALOG_ENTRIES = 1024;
+  private static final long MAX_CATALOG_ENTRIES = 1024;
 
-    private static ClusteredMediaDriver clusteredMediaDriver;
+  private static final int MEMBER_COUNT = 3;
+  private static final String CLUSTER_MEMBERS = clusterMembersString();
 
-    public static void main(String args[])
-    {
-      // init cluster
-      init();
+  private static final String LOG_CHANNEL =
+      "aeron:udp?term-length=64k|control-mode=manual|control=localhost:55550";
+  private static final String ARCHIVE_CONTROL_REQUEST_CHANNEL =
+      "aeron:udp?term-length=64k|endpoint=localhost:8010";
+  private static final String ARCHIVE_CONTROL_RESPONSE_CHANNEL =
+      "aeron:udp?term-length=64k|endpoint=localhost:8020";
 
-      // create client
-      final JobClient jobClient = ZbClient.newClient().jobClient();
+  private static ClusteredMediaDriver[] clusteredMediaDrivers =
+      new ClusteredMediaDriver[MEMBER_COUNT];
+  private static ClusteredServiceContainer[] containers =
+      new ClusteredServiceContainer[MEMBER_COUNT];
 
-      // test
-      jobClient.createJob();
+  public static void main(String args[]) {
+    // init cluster
+    init();
 
-      jobClient.subscribe((job -> {
-        System.out.println("Received job on client.");
-      }));
+  // create client
+    final ZbClient zbClient = ZbClient.newClient("localhost:20110,localhost:20111,localhost:20112");
+    final JobClient jobClient = zbClient.jobClient();
 
-      while (true);
+    // test
+    jobClient.createJob();
 
-      // tear down
-//      tearDown();
-    }
+    jobClient.subscribe(
+        (job -> {
+          System.out.println("Received job on client.");
+        }));
+
+    waitUntilClose();
+
+    zbClient.close();
+    
+    // tear down
+    tearDown();
+  }
 
   private static void init() {
-    clusteredMediaDriver = ClusteredMediaDriver.launch(
-      new MediaDriver.Context()
-        .threadingMode(ThreadingMode.SHARED)
-        .termBufferSparseFile(true)
-        .errorHandler(Throwable::printStackTrace)
-        .dirDeleteOnStart(true),
-      new Archive.Context()
-        .maxCatalogEntries(MAX_CATALOG_ENTRIES)
-        .threadingMode(ArchiveThreadingMode.SHARED)
-        .deleteArchiveOnStart(true),
-      new ConsensusModule.Context()
-        .errorHandler(Throwable::printStackTrace)
-        .deleteDirOnStart(true));
 
-    // start up
-//    container = launchEchoService();
-//    ClusteredServiceContainer.launch(
-//      new ClusteredServiceContainer.Context()
-//        .clusteredService(new JobExpireService())
-//        .errorHandler(Throwable::printStackTrace)
-//        .deleteDirOnStart(true));
+    final String aeronDirName = CommonContext.getAeronDirectoryName();
 
-    ClusteredServiceContainer.launch(
-      new ClusteredServiceContainer.Context()
-        .clusteredService(new JobCreateService())
-        .errorHandler(Throwable::printStackTrace)
-        .deleteDirOnStart(true));
+    for (int i = 0; i < MEMBER_COUNT; i++) {
+
+      final String baseDirName = aeronDirName + "-" + i;
+
+      final AeronArchive.Context archiveCtx =
+          new AeronArchive.Context()
+              .controlRequestChannel(memberSpecificPort(ARCHIVE_CONTROL_REQUEST_CHANNEL, i))
+              .controlRequestStreamId(100 + i)
+              .controlResponseChannel(memberSpecificPort(ARCHIVE_CONTROL_RESPONSE_CHANNEL, i))
+              .controlResponseStreamId(110 + i)
+              .aeronDirectoryName(baseDirName);
+
+      clusteredMediaDrivers[i] =
+          ClusteredMediaDriver.launch(
+              new MediaDriver.Context()
+                  .aeronDirectoryName(baseDirName)
+                  .threadingMode(ThreadingMode.SHARED)
+                  .termBufferSparseFile(true)
+                  .errorHandler(Throwable::printStackTrace)
+                  .dirDeleteOnStart(true),
+              new Archive.Context()
+                  .maxCatalogEntries(MAX_CATALOG_ENTRIES)
+                  .aeronDirectoryName(baseDirName)
+                  .archiveDir(new File(baseDirName, "archive"))
+                  .controlChannel(archiveCtx.controlRequestChannel())
+                  .controlStreamId(archiveCtx.controlRequestStreamId())
+                  .localControlChannel("aeron:ipc?term-length=64k")
+                  .localControlStreamId(archiveCtx.controlRequestStreamId())
+                  .threadingMode(ArchiveThreadingMode.SHARED)
+                  .deleteArchiveOnStart(true),
+              new ConsensusModule.Context()
+                  .clusterMemberId(i)
+                  .clusterMembers(CLUSTER_MEMBERS)
+                  .aeronDirectoryName(baseDirName)
+                  .clusterDir(new File(baseDirName, "consensus-module"))
+                  .ingressChannel("aeron:udp?term-length=64k")
+                  .logChannel(memberSpecificPort(LOG_CHANNEL, i))
+                  .archiveContext(archiveCtx.clone())
+                  .errorHandler(Throwable::printStackTrace)
+                  .deleteDirOnStart(true));
+
+      containers[i] =
+          ClusteredServiceContainer.launch(
+              new ClusteredServiceContainer.Context()
+                  .aeronDirectoryName(baseDirName)
+                  .archiveContext(archiveCtx.clone())
+                  .clusteredServiceDir(new File(baseDirName, "service"))
+                  .clusteredService(new JobCreateService())
+                  .errorHandler(Throwable::printStackTrace)
+                  .deleteDirOnStart(true));
+    }
   }
 
   private static void tearDown() {
-//    CloseHelper.close(aeronCluster);
-//    CloseHelper.close(container);
-    CloseHelper.close(clusteredMediaDriver);
+    //    CloseHelper.close(aeronCluster);
+    //    CloseHelper.close(container);
+    Arrays.stream(clusteredMediaDrivers).forEach(CloseHelper::close);
 
-    if (null != clusteredMediaDriver)
-    {
-      clusteredMediaDriver.consensusModule().context().deleteDirectory();
-      clusteredMediaDriver.archive().context().deleteArchiveDirectory();
-      clusteredMediaDriver.mediaDriver().context().deleteAeronDirectory();
-    }
+    Arrays.stream(clusteredMediaDrivers)
+        .forEach(
+            driver -> {
+              CloseHelper.close(driver);
+              driver.mediaDriver().context().deleteAeronDirectory();
+            });
   }
 
+  private static String memberSpecificPort(final String channel, final int memberId) {
+    return channel.substring(0, channel.length() - 1) + memberId;
+  }
+
+  private static String clusterMembersString() {
+    final StringBuilder builder = new StringBuilder();
+
+    for (int i = 0; i < MEMBER_COUNT; i++) {
+      builder
+          .append(i) // id
+          .append(',').append("localhost:2011").append(i) // client
+          .append(',').append("localhost:2022").append(i) // member
+          .append(',').append("localhost:2033").append(i) // log
+          .append(',').append("localhost:801").append(i) // archive
+          .append('|');
+    }
+
+    builder.setLength(builder.length() - 1);
+
+    return builder.toString();
+  }
+
+  private static void waitUntilClose() {
+    try (Scanner scanner = new Scanner(System.in)) {
+      while (scanner.hasNextLine()) {
+        final String nextLine = scanner.nextLine();
+        if (nextLine.contains("close")) {
+          return;
+        }
+      }
+    }
+  }
 }
--- src/main/java/io/zeebe/aeron/client/ZbClient.java
+++ src/main/java/io/zeebe/aeron/client/ZbClient.java
@@ -1,36 +1,46 @@
 package io.zeebe.aeron.client;
 
 import io.aeron.cluster.client.AeronCluster;
+import io.aeron.driver.MediaDriver;
+
+import org.agrona.CloseHelper;
 import org.agrona.concurrent.NoOpLock;
 
 public class ZbClient {
 
-    private AeronCluster aeronCluster;
+  private AeronCluster aeronCluster;
+  private MediaDriver mediaDriver;
 
-    private final JobClient jobClient;
+  private final JobClient jobClient;
 
-    private ZbClient()
-    {
-        aeronCluster = connectToCluster();
+  private ZbClient(String endpoints) {
+    mediaDriver = launchMediaDriver();
+    aeronCluster = connectToCluster(endpoints);
 
-        jobClient = new JobClient(aeronCluster);
-    }
+    jobClient = new JobClient(aeronCluster);
+  }
 
-    public static ZbClient newClient() {
-        return new ZbClient();
-    }
+  public static ZbClient newClient(String endPoints) {
+    return new ZbClient(endPoints);
+  }
 
-    public JobClient jobClient() {
-        return jobClient;
-    }
+  public JobClient jobClient() {
+    return jobClient;
+  }
 
-    private static AeronCluster connectToCluster()
-    {
-        return AeronCluster.connect(
-                new AeronCluster.Context()
-                        .ingressChannel("aeron:udp")
-                        .clusterMemberEndpoints("localhost:9010", "localhost:9011", "localhost:9012")
-                        .lock(new NoOpLock()));
-    }
+  public void close() {
+    CloseHelper.close(aeronCluster);
+  }
 
+  private static AeronCluster connectToCluster(String endpoints) {
+    return AeronCluster.connect(
+        new AeronCluster.Context()
+            .ingressChannel("aeron:udp")
+            .clusterMemberEndpoints(endpoints.split(","))
+            .lock(new NoOpLock()));
+  }
+
+  private static MediaDriver launchMediaDriver() {
+    return MediaDriver.launch(new MediaDriver.Context());
+  }
 }
